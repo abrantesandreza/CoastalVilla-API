79ea91fa89aabbc31f173a080f0e341104f51731
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-08-28 12:44:09 -0300

    Get individual Villa

--- CoastalVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ CoastalVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -15,5 +15,11 @@ namespace CoastalVilla_VillaAPI.Controllers
         {
             return VillaStore.villaList;
         }
+
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        }
     }
 }
