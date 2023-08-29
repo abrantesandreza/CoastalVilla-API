b22e6d28787a673d54058b1c2ae720a7fb24396b
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-08-29 17:08:14 -0300

    HttpPut implemented

--- CoastalVilla_VillaAPI/Data/VillaStore.cs
+++ CoastalVilla_VillaAPI/Data/VillaStore.cs
@@ -5,8 +5,8 @@ namespace CoastalVilla_VillaAPI.Data
     public static class VillaStore
     {
         public static List<VillaDTO> villaList = new List<VillaDTO> {
-            new VillaDTO { Id = 1, Name = "Pool View" },
-            new VillaDTO { Id = 2, Name = "Beach View" }
+            new VillaDTO { Id = 1, Name = "Pool View", Occupancy = 4, Sqft = 100 },
+            new VillaDTO { Id = 2, Name = "Beach View", Occupancy = 3, Sqft = 300 }
             };
     }
 }
