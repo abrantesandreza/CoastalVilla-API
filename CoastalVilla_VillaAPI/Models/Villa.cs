7958fd7f2afccfdebdfeed4f7fd98f488f57f026
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-08-31 15:19:33 -0300

    Updating Database Models

--- CoastalVilla_VillaAPI/Models/Villa.cs
+++ CoastalVilla_VillaAPI/Models/Villa.cs
@@ -1,9 +1,18 @@
-﻿namespace CoastalVilla_VillaAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoastalVilla_VillaAPI.Models
 {
     public class Villa
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Details { get; set; }
+        public double Rate { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
         public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
     }
 }
