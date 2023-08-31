1967b7fdaa1d916bf51de87adbc56c10610631b7
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-08-31 15:31:27 -0300

    Connection String and DbSet Entity

---
+++ CoastalVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using CoastalVilla_VillaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoastalVilla_VillaAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
