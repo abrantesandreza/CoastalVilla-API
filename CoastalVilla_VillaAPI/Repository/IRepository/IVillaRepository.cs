d259a0fca574241745539288cbd203ae23136c12
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-09-04 15:54:08 -0300

    Adding Villa Repository Interface

---
+++ CoastalVilla_VillaAPI/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,14 @@
+﻿using CoastalVilla_VillaAPI.Models;
+using System.Linq.Expressions;
+
+namespace CoastalVilla_VillaAPI.Repository.IRepository
+{
+    public interface IVillaRepository
+    {
+        Task<List<Villa>> GetAll(Expression<Func<Villa>> filter = null);
+        Task<Villa> Get(Expression<Func<Villa>> filter = null, bool tracked = true);
+        Task Create(Villa entity);
+        Task Remove(Villa entity);
+        Task Save();
+    }
+}
