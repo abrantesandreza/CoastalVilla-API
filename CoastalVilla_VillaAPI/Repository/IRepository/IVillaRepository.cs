ac0f1945ccd62f4afaaa8fe33a6cbe6df7871a1e
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-09-04 17:03:57 -0300

    VillaRepository implementation

--- CoastalVilla_VillaAPI/Repository/IRepository/IVillaRepository.cs
+++ CoastalVilla_VillaAPI/Repository/IRepository/IVillaRepository.cs
@@ -5,8 +5,8 @@ namespace CoastalVilla_VillaAPI.Repository.IRepository
 {
     public interface IVillaRepository
     {
-        Task<List<Villa>> GetAll(Expression<Func<Villa>> filter = null);
-        Task<Villa> Get(Expression<Func<Villa>> filter = null, bool tracked = true);
+        Task<List<Villa>> GetAll(Expression<Func<Villa, bool>> filter = null);
+        Task<Villa> Get(Expression<Func<Villa, bool>> filter = null, bool tracked = true);
         Task Create(Villa entity);
         Task Remove(Villa entity);
         Task Save();
