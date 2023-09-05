32796357725deb35da8aabf0844bf68717da25bc
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-09-05 12:56:38 -0300

    Separating VillaRepository and Repository db logics

---
+++ CoastalVilla_VillaAPI/Repository/IRepository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using CoastalVilla_VillaAPI.Models;
+using System.Linq.Expressions;
+
+namespace CoastalVilla_VillaAPI.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true);
+        Task CreateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task SaveAsync();
+    }
+}
