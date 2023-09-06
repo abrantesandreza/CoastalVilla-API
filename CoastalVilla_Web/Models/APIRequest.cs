165ef73e6a999ed3303ac7b250f9fd50a66f953c
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-09-06 14:38:02 -0300

    Web Project DTO's and API Models

---
+++ CoastalVilla_Web/Models/APIRequest.cs
@@ -0,0 +1,11 @@
+﻿using static CoastalVilla_Utility.SD;
+
+namespace CoastalVilla_Web.Models
+{
+    public class APIRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+    }
+}
