9450d4b509a3778fb262e699b5e95ee490fafda2
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-09-11 11:03:41 -0300

    Base Service for API

---
+++ CoastalVilla_Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using CoastalVilla_Web.Models;
+
+namespace CoastalVilla_Web.Services.IServices
+{
+    public interface IBaseService
+    {
+        APIResponse responseModel { get; set; }
+        Task<T> SendAsync<T>(APIRequest apiRequest);
+    }
+}
