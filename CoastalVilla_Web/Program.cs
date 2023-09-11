18d4ec1e160492751c91fb670526f332738c2462
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-09-11 12:17:53 -0300

    Adding Villa Service

--- CoastalVilla_Web/Program.cs
+++ CoastalVilla_Web/Program.cs
@@ -1,10 +1,15 @@
 using CoastalVilla_Web;
+using CoastalVilla_Web.Services.IServices;
+using CoastalVilla_Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddAutoMapper(typeof(MappingConfig));
+
+builder.Services.AddHttpClient<IVillaService, VillaService>();
+builder.Services.AddScoped<IVillaService, VillaService>();
 
 var app = builder.Build();
 
