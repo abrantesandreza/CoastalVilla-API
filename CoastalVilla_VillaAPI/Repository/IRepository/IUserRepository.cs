e6b9353ab5dc3b3f2aa10316566903bb6faf7f23
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-10-02 16:43:30 -0300

    Add UserRepository

---
+++ CoastalVilla_VillaAPI/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using CoastalVilla_VillaAPI.Models;
+using CoastalVilla_VillaAPI.Models.Dto;
+
+namespace CoastalVilla_VillaAPI.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO);
+    }
+}
---
+++ CoastalVilla_VillaAPI/Repository/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using CoastalVilla_VillaAPI.Data;
+using CoastalVilla_VillaAPI.Models;
+using CoastalVilla_VillaAPI.Models.Dto;
+using CoastalVilla_VillaAPI.Repository.IRepository;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace CoastalVilla_VillaAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
