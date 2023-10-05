cddf623262f36c55cbe4cc7ee3712c3cb9886fc3
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-10-05 13:06:23 -0300

    Adding AuthService

---
+++ CoastalVilla_Web/Services/AuthService.cs
@@ -0,0 +1,39 @@
+﻿using CoastalVilla_Utility;
+using CoastalVilla_Web.Models;
+using CoastalVilla_Web.Models.Dto;
+using CoastalVilla_Web.Services.IServices;
+
+namespace CoastalVilla_Web.Services
+{
+    public class AuthService : BaseService, IAuthService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private string villaUrl;
+
+        public AuthService(IHttpClientFactory clientFactory, IConfiguration configuration) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+            villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+        }
+
+        public Task<T> LoginAsync<T>(LoginRequestDTO obj)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = obj,
+                Url = villaUrl + "/api/UsersAuth/login"
+            });
+        }
+
+        public Task<T> RegisterAsync<T>(RegistrationRequestDTO obj)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = obj,
+                Url = villaUrl + "/api/UsersAuth/register"
+            });
+        }
+    }
+}
---
+++ CoastalVilla_Web/Services/IServices/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using CoastalVilla_Web.Models.Dto;
+
+namespace CoastalVilla_Web.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<T> LoginAsync<T>(LoginRequestDTO objToCreate);
+        Task<T> RegisterAsync<T>(RegistrationRequestDTO objToCreate);
+    }
+}
