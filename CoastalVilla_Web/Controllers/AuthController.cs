1c00b23332b4aac043c94647a3401d4f6c1aa8e4
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-10-05 13:26:45 -0300

    Implemented AuthController with Login and Register methods, Logout and AccessDenied to be implemented

---
+++ CoastalVilla_Web/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using CoastalVilla_Web.Models;
+using CoastalVilla_Web.Models.Dto;
+using CoastalVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoastalVilla_Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDTO obj = new();
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginRequestDTO obj)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegistrationRequestDTO obj)
+        {
+            APIResponse result = await _authService.RegisterAsync<APIResponse>(obj);
+            if (result != null && result.IsSuccess)
+            {
+                return RedirectToAction("login");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
